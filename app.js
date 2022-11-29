4d103c69e1f708bb0167d1e36b770173e1f2c799
Richard Choi <richardchoi54@gmail.com>
2022-11-29 16:04:48 -0500

    removed line in server that was causing error in render

--- app.js
+++ app.js
@@ -65,13 +65,6 @@ app.use(session({
     //cookie: { secure: true }
 }))
 
-if (process.env.NODE_ENV === 'production') {
-    //*Set static folder up in production
-    app.use(express.static('client/build'));
-
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','home.ejs')));
-  }
-
 //Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
