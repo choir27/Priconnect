c0cebf0ea1fbd58e86dbc27155652fd4ca759d27
Richard <richardchoi54@gmail.com>
2024-01-12 15:25:49 -0500

    start working on adding subscribe/unsubscribe/block/visit your account hub to replies

--- src/hooks/Post/RenderPosts/Comments/renderReplies/renderReplies.tsx
+++ src/hooks/Post/RenderPosts/Comments/renderReplies/renderReplies.tsx
@@ -4,7 +4,12 @@ import {
 } from "../../../../../middleware/Interfaces";
 import ReplyOptions from "../manageReplies/replyOptions";
 import PaginatedNav from "../../../../../components/PaginatedNav";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { ApiContext } from "../../../../../middleware/Context";
+import { Button, ButtonLink } from "../../../../../components/Button";
+import { getEmail } from "../../../../../middleware/Sessions";
+import GetSubscribedPosts from "../../Posts/PostFunctions/GetSubscribedPosts";
+import BlockAccount from "../../../../Account/manageAccount/blockAccount";
 
 export default function RenderReplies(props: CommentOptionsInterface) {
   if (
@@ -15,6 +20,9 @@ export default function RenderReplies(props: CommentOptionsInterface) {
     JSON.parse(props.post.comments[props.index]).replies.length
   ) {
     const [currentPage, setCurrentPage] = useState(1);
+    const [optionDisplay, setOptionDisplay] = useState<boolean>(false);
+
+    const { user } = useContext(ApiContext);
     const rowsPerPage = 2;
 
     const startIndex = (currentPage - 1) * rowsPerPage;
@@ -28,6 +36,40 @@ export default function RenderReplies(props: CommentOptionsInterface) {
           <article
             key={`${reply.id} ${reply.comment} ${props.post.$id} ${props.post.likes} ${props.post.$createdAt} ${props.post.$updatedAt} ${props.post.text}`}
           >
+            {Button({
+              text: "",
+              classNames: "fa-solid fa-ellipsis-vertical button2",
+              onClick: () => setOptionDisplay(!optionDisplay),
+            })}
+
+            {optionDisplay ? (
+              reply.id !== user.email || reply.id !== getEmail() ? (
+                <div className="flex displayOptions alignCenter justifyCenter">
+                  {reply.id === user.email || reply.id === getEmail()
+                    ? ""
+                    : Button({
+                        text: `Block ${reply.id.split("@")[0]}`,
+                        classNames: "button2",
+                        onClick: () => BlockAccount(reply.id, user.email),
+                      })}
+                </div>
+              ) : (
+                <div
+                  id="replyButtons"
+                  className="flex displayOptions alignCenter justifyCenter"
+                >
+                  {ButtonLink({
+                    domain: "/account",
+                    text: "Your Account",
+                    onClick: () => "",
+                    classNames: "button2",
+                  })}
+                </div>
+              )
+            ) : (
+              ""
+            )}
+
             <p>{reply.comment}</p>
             <ReplyOptions
               {...{ post: props.post, index: props.index, replyIndex: index }}
